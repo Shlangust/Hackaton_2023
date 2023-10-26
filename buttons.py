231422fb399e6814ca390f978eb850fb6d3d0e59
Shlangust <145592233+Shlangust@users.noreply.github.com>
2023-10-26 13:24:15 +0300

    added inline buttons

--- buttons.py
+++ buttons.py
@@ -22,9 +22,21 @@ async def button_check(message: types.message):
     if message.text == 'помощь':
         await message.answer('test1')
     if message.text == 'Рассписание':
-        await  message.answer('введите группу')
-          # if message.text in ОБРАЩЕНИЕ К БД
-          #      await message.answer(ВЫВОД ПАР)
+
+        rikb = InlineKeyboardMarkup(row_width=2)
+        rib1 = InlineKeyboardButton(text='Рассписание пар', callback_data='0')
+        rib2 = InlineKeyboardButton(text='Рассписание преподователей', callback_data='1')
+        rikb.add(rib1, rib2)
+        await message.answer(text='выберете опцию',reply_markup=rikb)
+
+
+
+@dp.callback_query_handler(text='0')
+async def std_replue(callback: types.CallbackQuery):
+    await callback.message.edit_text('введите группу')
+@dp.callback_query_handler(text='1')
+async def std_replue(callback: types.CallbackQuery):
+    await callback.message.edit_text('Введите Фамилию И О преподователя')
 
 
 if __name__ == '__main__':
